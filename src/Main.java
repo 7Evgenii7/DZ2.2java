970097be79f31288b604bcae03f814256a1c7d3c
Evgeniy <mikhailovich21@bk.ru>
2022-08-12 13:55:36 +0500

    Комит файлов

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int customerAccount = 1500;
+        int bonusMoney = 100;
+        int bonus = customerAccount/bonusMoney;
+        if (customerAccount>1099)
+        System.out.println("Начисленные бонусные рубли" + bonus);
+    }
+}
