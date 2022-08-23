4bae85a7a02d63472a6c80f921beb3341b9188e4
Evgeniy <mikhailovich21@bk.ru>
2022-08-23 15:13:35 +0500

    Коммит

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int customerAccount = 1500;
+        int customerAccount = 1180;
         int bonusMoney = 100;
-        int bonus = customerAccount/bonusMoney;
-        if (customerAccount>1099)
-        System.out.println("Начисленные бонусные рубли" + bonus);
+        int bonus = customerAccount / bonusMoney;
+        if (customerAccount > 1099)
+            System.out.println("Начисленные бонусные рубли" + bonus);
     }
 }
